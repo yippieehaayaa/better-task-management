d37ecc9a068a15bdcc93421a3ad55a017d38c99f
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-02-19 21:43:34 +0800

    init

---
+++ apis/iam/src/config.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "production", "test"]),
+  PORT: z.coerce.number().default(3000),
+});
+
+const env = envSchema.parse({
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+});
+
+export { env };
