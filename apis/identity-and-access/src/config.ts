6cd07716201e2615d130f3f96c4ba6b6892abac2
Joshua Dave Oropilla <joshdave900@gmail.com>
2026-02-21 15:57:29 +0800

    Refactor environment variable schema in config.ts: make NODE_ENV optional and retain default PORT value.

--- apis/identity-and-access/src/config.ts
+++ apis/identity-and-access/src/config.ts
@@ -4,15 +4,13 @@ import { z } from "zod";
 dotenv.config();
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["development", "production", "test"])
-    .default("development"),
-  PORT: z.coerce.number().default(3000),
+	NODE_ENV: z.enum(["development", "production", "test"]).optional(),
+	PORT: z.coerce.number().default(3000),
 });
 
 const env = envSchema.parse({
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
+	NODE_ENV: process.env.NODE_ENV,
+	PORT: process.env.PORT,
 });
 
 export { env };
